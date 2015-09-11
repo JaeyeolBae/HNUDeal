f434c3c947b8b4db53980fe5face479a8f23dbe5
lhw <hw3663@naver.com>
2015-09-11 11:22:14 +0900

    테스트2

--- app/src/main/java/com/hnudeal/main/MainActivity.java
+++ app/src/main/java/com/hnudeal/main/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
@@ -39,4 +40,6 @@ public class MainActivity extends AppCompatActivity {
     public void onButton1Clicked(View v) {
 
     }
+
+
 }
